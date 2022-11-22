9b69f58086934da1c49b36c04e25bfb43b948f4e
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-22 11:17:36 -0800

    Update wheresThePointer.js

--- wheresThePointer.js
+++ wheresThePointer.js
@@ -1,3 +1,12 @@
 // Attach one listener that will detect clicks on any of the <td>
 // elements.  Should update that element's innerHTML to be the
 // x, y coordinates of the mouse at the time of the click
+
+let square = document.querySelectorAll('td')
+for(let i =0;i<square.length;i++){
+    square[i].addEventListener('click', (e)=>{
+        let x = e.clientX
+        let y = e.clientY
+        square[i].innerHTML = `(${x} , ${y})`
+    })
+}
