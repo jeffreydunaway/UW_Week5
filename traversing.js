86f6a9407259b6dd61cccc491ae3f1e6f6ffd956
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-22 11:23:49 -0800

    Update traversing.js

--- traversing.js
+++ traversing.js
@@ -2,10 +2,14 @@
 // access the <main> node and log to the console.
 const body = document.querySelector('body');
 
+console.log(body.querySelector('main'))
+
 // Given the <ul> element as variable ul,
 // access <body>  and log to the console.
 const ul = document.querySelector('ul');
-
+console.log(ul.parentNode.parentNode)
 // Given the <p> element as var p,
 // access the 3rd <li>  and log to the console.
 const p = document.querySelector('p');
+let baby1 = p.parentElement.childNodes[3].hasChildNodes[5]
+console.log(baby1)
