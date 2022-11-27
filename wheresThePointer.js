66294fc84bcefb46a2fd80bcd2954a18db2a8867
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-27 11:49:31 -0800

    Update wheresThePointer.js

--- wheresThePointer.js
+++ wheresThePointer.js
@@ -2,11 +2,11 @@
 // elements.  Should update that element's innerHTML to be the
 // x, y coordinates of the mouse at the time of the click
 
-let square = document.querySelectorAll('td')
-for(let i =0;i<square.length;i++){
-    square[i].addEventListener('click', (e)=>{
+let box = document.querySelectorAll('td')
+for(let i =0;i<box.length;i++){
+    box[i].addEventListener('click', (e)=>{
         let x = e.clientX
         let y = e.clientY
-        square[i].innerHTML = `(${x} , ${y})`
+        box[i].innerHTML = `(${x} , ${y})`
     })
 }
