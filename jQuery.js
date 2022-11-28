c2d60096d0e573388fa939f6b1018ec4e0b9c67a
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-11-27 17:37:51 -0800

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -1,4 +1,5 @@
-// Change the class of the second <li> from to "sun" to "cloudy"
+//Figured I'd add this since we just did this in class. :) 
+//Change the class of the second <li> from to "sun" to "cloudy"
 $(document).ready(
     function(){
         let y = $('#weather li:nth-child(2)')
