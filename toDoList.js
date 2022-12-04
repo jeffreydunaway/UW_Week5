b8c14a27d31c98d20dd9a327c2b1340016f5c91b
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-12-04 11:33:57 -0800

    Update toDoList.js

--- toDoList.js
+++ toDoList.js
@@ -10,38 +10,44 @@ for(let i = 0;i<array.length;i++){
 
 // If a delete link is clicked, delete the li element / remove from the DOM
 
-let erase = document.getElementsByClassName('erase')
-console.log(erase)
+let remove = document.getElementsByClassName('delete')
+console.log(remove)
 
 for(let i = 0;i<array.length;i++){
-  for(let j =0; j<erasae.length;j++){
-    erase[i].addEventListener('click', ()=>{
-      array[i].erase()
+  for(let j =0; j<remove.length;j++){
+    remove[i].addEventListener('click', ()=>{
+      array[i].remove()
     })
   }
 }
-
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
 // Make sure to add an event listener(s) to your new <li> (if needed)
-const addListItem = function(q) {
-  q.preventDefault();
+const addListItem = function(e) {
+  e.preventDefault();
   const input = this.parentNode.getElementsByTagName('input')[0];
-  const text = input.value;
-   // use this text to create a new <li>
+  const text = input.value; // use this text to create a new <li>
 
+    
   // Finish function here
-const todayList = document.querySelector('.today-list');
-const li = document.createElement('li');
-const spanTag = document.createElement('span');
-spanTag.textContent = text + " ";
-const aTag = document.createElement('a');
-aTag.textContent = 'Delete'
-aTag.setAttribute('class', 'delete');
-li.append(spanTag);
-li.append(aTag);
-console.log(li)
-todayList.append(li);
+
+  const todayList = document.querySelector('.today-list');
+  const li = document.createElement('li');
+  const spanTag = document.createElement('span');
+  spanTag.textContent = text + " ";
+  const aTag = document.createElement('a');
+  aTag.textContent = 'Delete'
+  aTag.setAttribute('class', 'delete');
+  li.append(spanTag);
+  li.append(aTag);
+  console.log(li)
+  todayList.append(li);
 };
+
 const addList = document.getElementsByClassName('add-item')[0]
-addList.addEventListener('click',addListItem) 
+// addList.addEventListener('click', ()=>{
+//   test = new addListItem()
+//   console.log(test)
+// })
+
+addList.addEventListener('click',addListItem)
