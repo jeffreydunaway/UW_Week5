40a36d9471f54b2d13cfc3683ae94188c7e8ff27
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-12-03 21:50:49 -0800

    Update accessing.js

--- accessing.js
+++ accessing.js
@@ -1,21 +1,18 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
 
-const headline = document.getElementById('weather-head')
-headline.innerText = 'February 10 Weather Forecast, Seattle'
+const weatherHead = document.getElementById('weather-head');
+weatherHead.innerHTML = 'February 10 Weather Forecast, Seattle'
 
 // Change the styling of every element with class "sun" to set the color to "orange"
 const sunEls = document.querySelectorAll('.sun')
 
-sunEls.forEach((el) => el.style.color = 'orange')
- 
-// Change the class of the second <li> from to "sun" to "cloudy"
-//creating a loop through the array, using the ternary. have to create the class first then 
-// log it, and then loop it. :)
 let classes = document.getElementsByClassName("sun")
 console.log(classes)
 for(let i =0; i<classes.length; i++){
     classes[i].setAttribute('style', 'color:orange')
 }
+ 
+// Change the class of the second <li> from to "sun" to "cloudy"
 // see jQuery file
 
 const bubbie2 = document.querySelector('#weather :nth-child(2)');
