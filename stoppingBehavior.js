d97910ba40b648f262d7d492eb209c077d3d8f00
Jeffrey Dunaway <jeffreydunaway@users.noreply.github.com>
2022-12-06 10:12:27 -0800

    Update stoppingBehavior.js

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -6,15 +6,16 @@ document.getElementById('cat').addEventListener('click', () => {
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
 function switchToAlert(){
-  let theBoom = document.getElementById('more-info')
-  theBoom.setAttribute('href', '#')
+  let aTag = document.getElementById('more-info')
+  aTag.setAttribute('href', '#')
   alert("Here's some info")
 }
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
-const woof = document.getElementById('dog').addEventListener('click', event => {
+
+const killCat = document.getElementById('dog').addEventListener('click', event => {
   event.stopPropagation()
-  alert(`Bow wow wow yippy yo yippy yay
+  alert(`Bow wow wow yippy yo yippy yay 
   Doggy Dogg's in the ... house`);
     alert("Sorry... couldn't miss the oppurtunity ...its an old 90's song")
 })
